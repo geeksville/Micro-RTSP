53284101a2442a954695ef0a27e00e45d2d7a9ce
Kevin Hester <kevinh@geeksville.com>
2019-02-10 15:47:04 -0800

    Fix ESP32-CAM (aithinker) support
    @bbbenji yep I broke ESP32-CAM when I later started working with the other
    boards.  I just fixed it.  Can you confirm it works for you?  I'll also add
    some wiring instructions in the TenDollarWebcam git repo.
    
    Fixes #3

--- src/OV2640.cpp
+++ src/OV2640.cpp
@@ -36,6 +36,41 @@ camera_config_t esp32cam_config {
     .fb_count = 2 // if more than one i2s runs in continous mode.  Use only with jpeg
 };
 
+camera_config_t esp32cam_aithinker_config {
+
+    .pin_pwdn = 32,
+    .pin_reset = -1,
+
+    .pin_xclk = 0,
+
+    .pin_sscb_sda = 26,
+    .pin_sscb_scl = 27,
+
+    // Note: LED GPIO is apparently 4 not sure where that goes
+    // per https://github.com/donny681/ESP32_CAMERA_QR/blob/e4ef44549876457cd841f33a0892c82a71f35358/main/led.c
+    .pin_d7 = 35,
+    .pin_d6 = 34,
+    .pin_d5 = 39,
+    .pin_d4 = 36,
+    .pin_d3 = 21,
+    .pin_d2 = 19,
+    .pin_d1 = 18,
+    .pin_d0 = 5,
+    .pin_vsync = 25,
+    .pin_href = 23,
+    .pin_pclk = 22,
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_1,
+    .ledc_channel = LEDC_CHANNEL_1,
+    .pixel_format = PIXFORMAT_JPEG,
+    // .frame_size = FRAMESIZE_UXGA, // needs 234K of framebuffer space
+    // .frame_size = FRAMESIZE_SXGA, // needs 160K for framebuffer
+    // .frame_size = FRAMESIZE_XGA, // needs 96K or even smaller FRAMESIZE_SVGA - can work if using only 1 fb
+    .frame_size = FRAMESIZE_SVGA,
+    .jpeg_quality = 12,               //0-63 lower numbers are higher quality
+    .fb_count = 2 // if more than one i2s runs in continous mode.  Use only with jpeg
+};
+
 camera_config_t esp32cam_ttgo_t_config {
 
     .pin_pwdn = 26,
@@ -145,16 +180,10 @@ esp_err_t OV2640::init(camera_config_t config)
     memset(&_cam_config, 0, sizeof(_cam_config));
     memcpy(&_cam_config, &config, sizeof(config));
 
-    //power up the camera if PWDN pin is defined
-    if(config.pin_pwdn != -1) {
-        pinMode(config.pin_pwdn, OUTPUT);
-        digitalWrite(config.pin_pwdn, LOW);
-    }
-
     esp_err_t err = esp_camera_init(&_cam_config);
     if (err != ESP_OK)
     {
-        ESP_LOGD(TAG, "Camera probe failed with error 0x%x", err);
+        printf("Camera probe failed with error 0x%x", err);
         return err;
     }
     // ESP_ERROR_CHECK(gpio_install_isr_service(0));
